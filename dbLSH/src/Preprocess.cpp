c719faca1d5002fc4710c44e61e4b2bd0f383b4d
samin_yeaser <samin.yeaser@tigerit.com>
2024-02-06 17:29:59 -0500

    Fixed bug

--- dbLSH/src/Preprocess.cpp
+++ dbLSH/src/Preprocess.cpp
@@ -107,7 +107,7 @@ void Preprocess::load_data(const std::string& path)
 	for (int i = 0; i < data.numQuery; ++i) {
 		data.query[i] = new float[data.dim];
 		for (int j=0;j<query[i].size();j++) {
-			data.val[i][j]=query[i][j];
+			data.query[i][j]=query[i][j];
 		}
 	}
 	for (int i = 0; i < data.numQuery; ++i) {
