1616a224cbca42e4a9d5be6a75cda6655ddf2433
samin_yeaser <samin.yeaser@tigerit.com>
2024-03-13 06:58:00 -0400

    added some scripts

---
+++ dbLSH/scripts/20-1.sh
@@ -0,0 +1,79 @@
+k=20
+
+declare -a arr=("glove", "audio", "cifar", "deep", "enron")
+
+for datasetName in "${arr[@]}"
+do
+    c=1.5
+    L=5
+    K=10
+    beta=0.1
+    R_min=6.50
+
+    ./dblsh $datasetName $c $k $L $K $beta $R_min
+
+    c=1.75
+    L=5
+    K=10
+    beta=0.1
+    R_min=6.50
+
+    ./dblsh $datasetName $c $k $L $K $beta $R_min
+
+    c=1.25
+    L=5
+    K=10
+    beta=0.1
+    R_min=6.50
+
+    ./dblsh $datasetName $c $k $L $K $beta $R_min
+
+    c=1.5
+    L=5
+    K=10
+    beta=0.1
+    R_min=300
+
+    ./dblsh $datasetName $c $k $L $K $beta $R_min
+
+    c=1.5
+    L=5
+    K=10
+    beta=0.1
+    R_min=600
+
+    ./dblsh $datasetName $c $k $L $K $beta $R_min
+
+    c=1.5
+    L=10
+    K=10
+    beta=0.1
+    R_min=6.50
+
+    ./dblsh $datasetName $c $k $L $K $beta $R_min
+
+    c=1.5
+    L=15
+    K=10
+    beta=0.1
+    R_min=6.50
+
+    ./dblsh $datasetName $c $k $L $K $beta $R_min
+
+    c=1.5
+    L=5
+    K=12
+    beta=0.1
+    R_min=6.50
+
+    ./dblsh $datasetName $c $k $L $K $beta $R_min
+
+    c=1.5
+    L=5
+    K=14
+    beta=0.1
+    R_min=6.50
+
+    ./dblsh $datasetName $c $k $L $K $beta $R_min
+
+done
